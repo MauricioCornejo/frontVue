aa5e33119678229154ebf70b56a7cbc2be88917e
Mauricio <mauricio.cornejo@orbisdata.cl>
2021-06-22 19:29:24 -0400

    init

---
+++ frontend/src/views/Chat.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="home">
+    <!-- <img alt="Vue logo" src="../assets/logo.png"> -->
+    <top-bar>CHAT.VUE CHAT SELECCIONADO</top-bar>
+    <chat-list />
+    <!-- <new-chat /> -->
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import TopBar from '@/components/TopBar.vue';
+import ChatList from '@/components/ChatList.vue';
+
+export default {
+  name: 'home',
+  components: {
+    TopBar,
+    ChatList,
+  },
+};
+</script>
